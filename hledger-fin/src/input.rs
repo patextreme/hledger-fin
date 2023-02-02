ff260fee179778720b2184f806d7385ded1c550c
Pat Losoponkul <patextreme@hotmail.com>
2023-02-03 01:11:25 +0700

    Add error handling when import resources

--- hledger-fin/src/input.rs
+++ hledger-fin/src/input.rs
@@ -1,9 +1,32 @@
+use std::path::Path;
+
 use crate::model::{
     port::CashBalancePortfolio,
     txn::{Buy, Deposit, Sell, Withdraw},
     Commodity,
 };
 use serde::{Deserialize, Serialize};
+use serde_yaml::Value;
+
+#[derive(thiserror::Error, Debug)]
+pub enum ImportError {
+    #[error("unable to read file: {0}")]
+    FileIO(#[from] std::io::Error),
+    #[error("unable to parse resource: {0}")]
+    ParseError(String),
+}
+
+pub fn from_file<P: AsRef<Path>>(path: P) -> Result<Vec<Resource>, ImportError> {
+    let yaml = std::fs::read_to_string(path.as_ref())?;
+    let mut resources = Vec::new();
+    for doc in serde_yaml::Deserializer::from_str(&yaml) {
+        let value = Value::deserialize(doc).map_err(|e| ImportError::ParseError(e.to_string()))?;
+        let r: Resource =
+            serde_yaml::from_value(value).map_err(|e| ImportError::ParseError(e.to_string()))?;
+        resources.push(r)
+    }
+    Ok(resources)
+}
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(tag = "kind", content = "spec")]
--- hledger-fin/src/main.rs
+++ hledger-fin/src/main.rs
@@ -1,18 +1,11 @@
-use crate::input::Resource;
-use serde::Deserialize;
-use serde_yaml::Value;
-use std::fs;
-
 mod input;
 mod inventory;
 mod model;
 
 fn main() {
-    let input_yaml = fs::read_to_string("./examples/resources.yaml").unwrap();
-    for doc in serde_yaml::Deserializer::from_str(&input_yaml) {
-        let value = Value::deserialize(doc).unwrap();
-        let resource: Resource = serde_yaml::from_value(value).unwrap();
+    let resources = input::from_file("./examples/resources.yaml").unwrap();
+    for r in resources {
         println!("---");
-        println!("{resource:?}");
+        println!("{r:?}");
     }
 }
